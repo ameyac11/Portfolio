fad561c9879222c84a128dc054cca3b9be530a34
ameyac11 <ameyachopade402@gmail.com>
2026-06-28 11:18:34 +0530

    minor chnages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { createRoot } from 'react-dom/client'
+import clarity from '@microsoft/clarity'
 import App from './App.tsx'
 import './index.css'
+
+clarity.init('xdy2sa5z8r')
 
 // Wait for DOM load.
 document.addEventListener('DOMContentLoaded', () => {
